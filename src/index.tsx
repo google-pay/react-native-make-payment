00cf42b245600e9b93d8eb1e42244b7d09ff8084
Brook Jeynes <me@brookjeynes.dev>
2025-09-01 11:54:29 +1000

    fix: export default exports from NativeMakePaymentModule
    This fixes an issue when attempting to import types from the NativeMakePaymentsModule

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ export * from './PaymentRequest';
 export { GooglePayButton, GooglePayButtonConstants } from './GooglePayButton';
 export type * from './PaymentRequest';
 export type * from './specs/NativeMakePaymentModule';
+export { default } from './specs/NativeMakePaymentModule';
 
 const GOOGLE_PAY_PMI = 'google_pay';
 
