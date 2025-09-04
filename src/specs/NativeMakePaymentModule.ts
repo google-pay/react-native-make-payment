5c431e3d86f05ab6d0b74779e2b8bea3f09fb31c
brookjeynes <me@brookjeynes.dev>
2025-09-05 09:57:24 +1000

    feat: semi-type field `info` in `GooglePayPaymentMethodData`

--- src/specs/NativeMakePaymentModule.ts
+++ src/specs/NativeMakePaymentModule.ts
@@ -2,6 +2,80 @@ import type { TurboModule } from 'react-native';
 import { TurboModuleRegistry, NativeModules } from 'react-native';
 
 /**
+* This object provides information about what validation has been performed on the returned payment credentials so that appropriate instrument risk checks can be applied.
+* https://developers.google.com/pay/api/web/reference/response-objects#assurance-details-specifications
+*/
+export type GooglePayAssuranceDetailsSpecifications = {
+  /**
+  * If `true`, indicates that `Cardholder` possession validation has been performed on returned payment credential.
+  */
+  accountVerified: boolean
+  /**
+  * - If `true`, indicates that identification and verifications (ID&V) was performed on the returned payment credential.
+  * - If `false`, the same risk-based authentication can be performed as you would for card transactions.
+  *   This risk-based authentication can include, but not limited to, step-up with 3D Secure protocol if applicable.
+  */
+  cardHolderAuthenticated: boolean
+}
+
+/**
+ * This object provides information about a requested postal address.
+ * https://developers.google.com/pay/api/web/reference/response-objects#Address
+ */
+export type GooglePayAddress = {
+  /** The full name of the addressee. */
+  name: string;
+  /** The postal or ZIP code. */
+  postalCode: string;
+  /** ISO 3166-1 alpha-2 country code. */
+  countryCode: string;
+  /**
+  * A telephone number, if phoneNumberRequired is set to true in the PaymentDataRequest.
+  */
+  phoneNumber: string;
+  /** The first line of the address. */
+  address1: string;
+  /** The second line of the address. */
+  address2: string;
+  /** The third line of the address. */
+  address3: string;
+  /** City, town, neighborhood, or suburb. */
+  locality: string;
+  /** A country subdivision, such as a state or province. */
+  administrativeArea: string;
+  /** The sorting code. */
+  sortingCode: string;
+}
+
+/**
+* This object provides information about the selected payment card.
+* https://developers.google.com/pay/api/web/reference/response-objects#CardInfo
+*/
+export type GooglePayCardInfo = {
+  /**
+  * The details about the card.
+  * This value is commonly the last four digits of the selected payment account number.
+  */
+  cardDetails: string
+  /**
+  * This object provides information about the validation performed on the returned payment data if `assuranceDetailsRequired` is set to true in the {@link https://developers.google.com/pay/api/web/reference/request-objects#CardParameters|CardParameters}.
+  */
+  assuranceDetails: GooglePayAssuranceDetailsSpecifications
+  /**
+  * The payment card network of the selected payment. Returned values match the format of `allowedCardNetworks` in {@link https://developers.google.com/pay/api/web/reference/request-objects#CardParameters|CardParameters}.
+  *
+  * This card network value should not be displayed to the buyer. It's used when the details of a buyer's card are needed.
+  * For example, if customer support needs this value to identify the card a buyer used for their transaction.
+  * For a user-visible description, use the description property of {@link GooglePayPaymentMethodData} instead.
+  */
+  cardNetwork: string;
+  /**
+  *	The billing address associated with the provided payment method, if billingAddressRequired is set to true in {@link https://developers.google.com/pay/api/web/reference/request-objects#CardParameters|CardParameters}.
+  */
+  billingAddress?: GooglePayAddress;
+}
+
+/**
 * This object provides tokenization data for the payment method.
 * https://developers.google.com/pay/api/web/reference/response-objects#PaymentMethodTokenizationData
 */
@@ -37,7 +111,7 @@ export type GooglePayPaymentMethodData = {
   * The value of this property depends on the payment method `type` returned.
   * For `CARD`, see {@link https://developers.google.com/pay/api/web/reference/response-objects#CardInfo|CardInfo}.
   */
-  info: unknown;
+  info: GooglePayCardInfo | object;
   /** Payment tokenization data for the selected payment method. */
   tokenizationData?: GooglePayPaymentMethodTokenizationData
 }
