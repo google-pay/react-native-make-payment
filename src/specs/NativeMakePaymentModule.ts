bf2bbe030f34f29d521b474fcb598dbf31f1a0e3
brookjeynes <me@brookjeynes.dev>
2025-09-05 09:41:09 +1000

    feat: add types for GooglePayPaymentData
    This struct was missing some types that were quite important. It also
    seems to include some extra types that don't actually appear to be there
    but i've left them for now just in-case there are future plans for
    these.
    
    Those fields being:
    - allowedPaymentMethods: GooglePayPaymentMethod[];
    - transactionInfo: GooglePayTransactionInfo;
    - merchantInfo: GooglePayMerchantInfo;

--- src/specs/NativeMakePaymentModule.ts
+++ src/specs/NativeMakePaymentModule.ts
@@ -1,9 +1,64 @@
 import type { TurboModule } from 'react-native';
 import { TurboModuleRegistry, NativeModules } from 'react-native';
 
+/**
+* This object provides tokenization data for the payment method.
+* https://developers.google.com/pay/api/web/reference/response-objects#PaymentMethodTokenizationData
+*/
+export type GooglePayPaymentMethodTokenizationData = {
+  /**
+  * The type of tokenization to be applied to the selected payment method.
+  * This value matches the `type` set in {@link https://developers.google.com/pay/api/web/reference/request-objects#PaymentMethodTokenizationSpecification|PaymentMethodTokenizationSpecification}`.
+  */
+  type: string;
+  /**
+  * The generated payment method token.
+  * - {@link https://developers.google.com/pay/api/web/reference/request-objects#gateway|PAYMENT_GATEWAY}: JSON object string that contains a chargeable token issued by your gateway.
+  * - {@link https://developers.google.com/pay/api/web/reference/request-objects#direct|DIRECT}: protocolVersion, signature, and a signedMessage for decryption.
+  *   See {@link https://developers.google.com/pay/api/web/guides/resources/payment-data-cryptography#payment-method-token-structure|Payment method token structure} for more information.
+  */
+  token?: string;
+}
+
+/**
+* This object provides data for a selected payment method.
+* https://developers.google.com/pay/api/web/reference/response-objects#PaymentMethodData
+*/
+export type GooglePayPaymentMethodData = {
+  /**
+  * {@link GooglePayPaymentMethod} `type` selected in the Google Pay payment sheet.
+  */
+  type: string;
+  /**
+  * User-facing message to describe the payment method that funds this transaction.
+  */
+  description: string;
+  /**
+  * The value of this property depends on the payment method `type` returned.
+  * For `CARD`, see {@link https://developers.google.com/pay/api/web/reference/response-objects#CardInfo|CardInfo}.
+  */
+  info: unknown;
+  /** Payment tokenization data for the selected payment method. */
+  tokenizationData?: GooglePayPaymentMethodTokenizationData
+}
+
+/**
+* This is a response object that's returned by Google after a payer approves payment.
+* https://developers.google.com/pay/api/web/reference/response-objects#PaymentData
+*/
 export type GooglePayPaymentData = {
+  /**
+  * Major API version.
+  * The value in the response matches the value provided in {@link https://developers.google.com/pay/api/web/reference/request-objects#PaymentDataRequest|PaymentDataRequest}.
+  */
   apiVersion: number;
+  /**
+  * Minor API version.
+  * The value in the response matches the value provided in {@link https://developers.google.com/pay/api/web/reference/request-objects#PaymentDataRequest|PaymentDataRequest}.
+  */
   apiVersionMinor: number;
+  /** Data about the selected payment method. */
+  paymentMethodData: GooglePayPaymentMethodData;
   allowedPaymentMethods: GooglePayPaymentMethod[];
   transactionInfo: GooglePayTransactionInfo;
   merchantInfo: GooglePayMerchantInfo;
