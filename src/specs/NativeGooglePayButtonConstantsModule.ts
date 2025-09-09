82f6eaeae2ea62945eb673469bfc6d77c795be2a
brookjeynes <me@brookjeynes.dev>
2025-09-10 08:47:07 +1000

    fix: `GooglePayButtonConstants` typings

--- src/specs/NativeGooglePayButtonConstantsModule.ts
+++ src/specs/NativeGooglePayButtonConstantsModule.ts
@@ -1,8 +1,25 @@
 import type { TurboModule } from 'react-native';
 import { TurboModuleRegistry, NativeModules } from 'react-native';
 
+export interface Constants {
+  Themes: {
+    Dark: number;
+    Light: number;
+  };
+  Types: {
+    Buy: number;
+    Book: number;
+    Checkout: number;
+    Donate: number;
+    Order: number;
+    Pay: number;
+    Subscribe: number;
+    Plain: number;
+  };
+}
+
 export interface Spec extends TurboModule {
-  loadConstants(): { string: { string: number } };
+  loadConstants(): Constants;
 }
 
 export default (
